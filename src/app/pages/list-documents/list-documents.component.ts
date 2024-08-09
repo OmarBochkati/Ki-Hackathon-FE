f9a542cb0cd74eb795eba0593fc790cc655b2564
Mohamad Omar Bouchkati <o.bouchkati@novobit.eu>
2024-08-09 13:12:40 +0200

    add chatbot to list page

--- src/app/pages/list-documents/list-documents.component.ts
+++ src/app/pages/list-documents/list-documents.component.ts
@@ -1,9 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {FileUploadService} from "../../services/file-upload.service";
 import {HttpEventType, HttpResponse} from "@angular/common/http";
-import {MatButton} from "@angular/material/button";
+import {MatButton, MatFabButton} from "@angular/material/button";
 import {RouterLink} from "@angular/router";
 import {MatCard, MatCardContent, MatCardSubtitle, MatCardTitle} from "@angular/material/card";
+import {ChatbotComponent} from "../../components/chatbot/chatbot.component";
+import {MatSuffix} from "@angular/material/form-field";
+import {NgClass, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-list-documents',
@@ -14,7 +17,12 @@ import {MatCard, MatCardContent, MatCardSubtitle, MatCardTitle} from "@angular/m
     MatCard,
     MatCardContent,
     MatCardTitle,
-    MatCardSubtitle
+    MatCardSubtitle,
+    ChatbotComponent,
+    MatFabButton,
+    MatSuffix,
+    NgIf,
+    NgClass
   ],
   templateUrl: './list-documents.component.html',
   styleUrl: './list-documents.component.scss'
@@ -38,5 +46,22 @@ export class ListDocumentsComponent implements OnInit{
         const msg = 'Error requesting files';
       }
     });
+  }
+
+
+  private readonly botIconPath = './assets/bot.png';
+  private readonly chatIconPath = './assets/chat.png';
+
+  public isOpen = false;
+  public iconSrc = this.botIconPath;
+  public iconState = 'default';
+
+
+  public onChangeChatState(): void {
+    this.isOpen = !this.isOpen;
+    this.iconState = (this.iconState === 'default' ? 'rotated' : 'default');
+
+    if (this.isOpen) this.iconSrc = this.chatIconPath;
+    else this.iconSrc = this.botIconPath;
   }
 }
